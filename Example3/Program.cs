049e3d8d50301450042acd943618e437105d7a78
Vladislav <andrushkovl@gmail.com>
2023-02-12 18:15:12 +0300

    Добавили 3 задачу про Find

---
+++ Example3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 11, 12, 31, 41, 151, 61, 71, 18, 41 };
+int n = array.Length;
+int find = 41;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
